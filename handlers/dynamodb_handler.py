0c26d4b99472672e5ea87b5ccd6d47bab8374ce0
Dr-Banana <zhangxuanzhi10419@gmail.com>
2024-07-01 12:40:21 -0700

    return to previous version

--- handlers/dynamodb_handler.py
+++ handlers/dynamodb_handler.py
@@ -17,30 +17,3 @@ class DynamoDBHandler:
             )
         except Exception as e:
             raise RuntimeError(f"Error saving to DynamoDB: {str(e)}")
-        
-    def get_ongoing_conversations(self):
-        try:
-            response = self.table.scan()
-            ongoing_conversations = [item['OngoingConversationID'] for item in response['Items'] if 'OngoingConversationID' in item]
-            return ongoing_conversations
-        except Exception as e:
-            raise RuntimeError(f"Error retrieving ongoing conversations from DynamoDB: {str(e)}")
-        
-    def update_ongoing_conversation(self, user_id, conversation_id):
-        try:
-            self.table.update_item(
-                Key={'UserID': user_id},
-                UpdateExpression="SET OngoingConversationID = :c",
-                ExpressionAttributeValues={':c': conversation_id}
-            )
-        except Exception as e:
-            raise RuntimeError(f"Error updating ongoing conversation in DynamoDB: {str(e)}")
-        
-    def remove_ongoing_conversation(self, user_id):
-        try:
-            self.table.update_item(
-                Key={'UserID': user_id},
-                UpdateExpression="REMOVE OngoingConversationID"
-            )
-        except Exception as e:
-            raise RuntimeError(f"Error removing ongoing conversation from DynamoDB: {str(e)}")
--- handlers/s3_handler.py
+++ handlers/s3_handler.py
@@ -1,4 +1,7 @@
+import boto3
 from botocore.exceptions import NoCredentialsError
+
+s3_client = boto3.client('s3')
 
 def download_json_from_s3(s3_client, bucket_name, file_name):
     try:
@@ -20,7 +23,7 @@ def upload_json_to_s3(s3_client, bucket_name, file_name, json_data):
         print(f"Error uploading file to S3: {e}")
         return False
 
-def save_to_s3(s3_client, bucket_name, key, data):
+def save_to_s3(bucket_name, key, data):
     try:
         s3_client.put_object(Bucket=bucket_name, Key=key, Body=data)
         return True
