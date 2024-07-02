11bbf62f98f6db9a6a8ac7e7905e2c07ec711722
Dr-Banana <zhangxuanzhi10419@gmail.com>
2024-07-02 13:23:17 -0700

    fix s3 read

--- handlers/s3_handler.py
+++ handlers/s3_handler.py
@@ -5,9 +5,9 @@ s3_client = boto3.client('s3')
 
 def download_json_from_s3(bucket_name, key):
     try:
-        response = s3_client.get_object(Bucket=bucket_name, Key=key)
-        json_data = response['Body'].read().decode('utf-8')
-        return response
+        response = s3_client.Object(bucket_name, key)
+        json_data = response.get()['Body'].read().decode('utf-8')
+        return json_data
     except Exception as e:
         print(f"Error downloading file from S3: {e}")
         return None
